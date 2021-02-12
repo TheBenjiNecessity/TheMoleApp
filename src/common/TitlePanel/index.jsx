6e4b86062eb01f1193210b7f036e8e09f5e8530d
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2021-02-11 23:03:13 -0500

    Rename "next panel"

---
+++ src/common/TitlePanel/index.jsx
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react';
+
+import './next-panel.scss';
+
+const TitlePanel = ({ titleText, children }) => {
+	return (
+		<Fragment>
+			<div className="panel centered-panel centered-panel-medium next-panel">
+				{titleText && (
+					<Fragment>
+						<h1 className="title col-sm-12">{titleText}</h1>
+						<hr />
+					</Fragment>
+				)}
+				{children}
+			</div>
+		</Fragment>
+	);
+};
+
+export default TitlePanel;
