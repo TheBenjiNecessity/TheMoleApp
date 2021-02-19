b5d3ab3e41fb35786f526064f51cf4b13f30602a
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2021-02-18 22:24:36 -0500

    Add inline player list styles

--- src/common/PlayerList.jsx
+++ src/common/PlayerList.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import PlayerListView from './PlayerListView';
 
-const PlayerList = ({ players, animate }) => {
+const PlayerList = ({ players, animate, inline }) => {
 	if (!players || !players.length) {
 		return null;
 	}
 
-	return players.map((player, i) => <PlayerListView animate={animate} key={i} player={player} />);
+	return players.map((player, i) => <PlayerListView animate={animate} key={i} player={player} inline />);
 };
 
 export default PlayerList;
--- src/common/PlayerListView/index.jsx
+++ src/common/PlayerListView/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import clsx from 'clsx';
 
 import './player-list-view.scss';
 
-const PlayerListView = ({ player, animate, className }) => {
+const PlayerListView = ({ player, animate, className, inline }) => {
 	const style = animate ? { animation: `expandIn 1s` } : null;
 
 	return (
-		<div className={className}>
+		<div className={clsx(className, inline && 'inline')}>
 			<div className="player-view" style={style}>
 				{player && player.name}
 			</div>
