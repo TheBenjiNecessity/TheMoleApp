7efb2cbe6b6048747b1d9f17be45d64550c5b023
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2021-02-20 22:11:17 -0500

    Add column widths

--- src/components/WelcomeView.jsx
+++ src/components/WelcomeView.jsx
@@ -5,7 +5,7 @@ import TitlePanel from '../common/TitlePanel';
 const WelcomeView = ({ room }) => {
 	return (
 		<TitlePanel titleText="Welcome">
-			<AgreePanel room={room} />
+			<AgreePanel room={room} xl={6} lg={6} md={6} sm={12} xs={12} />
 		</TitlePanel>
 	);
 };
