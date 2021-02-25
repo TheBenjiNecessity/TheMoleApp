7ff44bfd53387cf9d803650524f595a75bd9a2ba
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2021-02-24 22:24:35 -0500

    Change how elimination view works

--- src/components/ExecutionView.jsx
+++ src/components/ExecutionView.jsx
@@ -1,5 +1,7 @@
-import React, { useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import * as _ from 'lodash';
+import styles from './ExecutionView.module.scss';
+import clsx from 'clsx';
 
 const EXECUTION_STATE = {
 	START: 0, // Before showing the player input (Showing a message about seeing red/green screen)
@@ -10,131 +12,102 @@ const EXECUTION_STATE = {
 	SHOWING_RESULTS: 5 // The Red/Green results screen is showing
 };
 
-const NEXT_STATE = [
-	{
-		state: EXECUTION_STATE.PRE_SUBMIT_WAITING,
-		timeout: 20000
-	},
-	{
-		state: EXECUTION_STATE.SUBMITTING,
-		timeout: 5000
-	},
-	{
-		state: EXECUTION_STATE.POST_SUBMIT_WAITING,
-		timeout: 0
-	},
-	{
-		state: EXECUTION_STATE.LOADING_RESULTS,
-		timeout: 5000
-	},
-	{
-		state: EXECUTION_STATE.SHOWING_RESULTS,
-		timeout: 5000
-	},
-	{
-		state: EXECUTION_STATE.PRE_SUBMIT_WAITING,
-		timeout: 5000
-	}
-];
+const CHAR_INPUT_TIMES = [ 500, 750, 1000, 1500 ];
 
-const CHAR_INPUT_TIMES = [ 100, 250, 500, 1000 ];
-
-const ExecutionView = ({ players, eliminatedPlayer, currentPlayer }) => {
-	// Any messages to tell the players about what's going on will be in the post quiz step
-	// Randomize the list of players
-	// For each player in the random list:
-	// 		Wait a few moments
-	//		Show an input where the player's name is entered one character at a time
-	//			(This should be random and varied like a person typing in the letters)
-	//		What a few moments
-	//		Show the input being 'submitted' by hiding the input and showing a blank screen
-	//			(blank means only show the background image)
-	//		What a few moments (the amount of time should be random to ratch up tension)
-	//		Show a red/green screen based on whether the player was eliminated
-	//		if the screen was green:
-	//			Wait a few moments
-	//			hide the green screen
-	//			Wait a few moments
-	//			show the input again
-	//		else if the screen was red
-	//			Wait a few moments
-	//			hide the red screen
-	//			Wait a few moments
-	//			move to the execution wrap up
-
-	const [ playerList, setPlayerList ] = useState(_.shuffle(players));
-	const [ currentInputPlayer, setCurrentInputPlayer ] = useState(null);
+const ExecutionView = ({ shuffledPlayers, eliminatedPlayer }) => {
+	const [ currentShuffledPlayers, setCurrentShuffledPlayers ] = useState(shuffledPlayers);
+	const [ currentInputPlayer, setCurrentInputPlayer ] = useState(shuffledPlayers[0]);
 	const [ currentCharacterIndex, setCurrentCharacterIndex ] = useState(-1);
-	const [ currentCharacterTimeout, setCurrentCharacterTimeout ] = useState(getRandomInputTime());
+	const [ currentTick, setCurrentTick ] = useState(0);
 	const [ currentState, setCurrentState ] = useState(EXECUTION_STATE.START);
 	const [ inputValue, setInputValue ] = useState('');
 
-	const outerClassName = useMemo(
-		() => {
-			let result = 'execution';
-			if (currentState === EXECUTION_STATE.SHOWING_RESULTS) {
-				result += ' execution-end';
-				const hasSucceeded = currentInputPlayer.name !== eliminatedPlayer.name;
-				result += hasSucceeded ? ' success' : ' fail';
-			}
-
-			return result;
-		},
-		[ currentInputPlayer, currentState, eliminatedPlayer ]
+	const showStartParagraph = currentState === EXECUTION_STATE.START;
+	const showInput = _.includes(
+		[ EXECUTION_STATE.PRE_SUBMIT_WAITING, EXECUTION_STATE.SUBMITTING, EXECUTION_STATE.POST_SUBMIT_WAITING ],
+		currentState
 	);
 
-	function getRandomInputTime() {
-		return CHAR_INPUT_TIMES[_.random(CHAR_INPUT_TIMES.length)];
-	}
+	const setNextPlayer = useCallback(
+		() => {
+			const newShuffledPlayers = currentShuffledPlayers.slice(1);
 
-	if (currentPlayer) {
-		return (
-			<div className={outerClassName}>
-				{currentState === EXECUTION_STATE.START ? (
-					<p>
-						In a moment, each player's name will be input into the host computer one at a time. If you see a
-						green screen after your name has been entered you are safe for this episode. If, however, you
-						see a red screen after your name has been entered then you have been eliminated from the game.
-					</p>
-				) : (
-					<p>Watch the host screen for which player is eliminated.</p>
-				)}
-			</div>
-		);
-	} else {
-		const next = NEXT_STATE[currentState];
-		if (next.timeout) {
-			setTimeout(() => {
-				setCurrentState(next.state);
-			}, next.timeout);
-		} else if (currentState === EXECUTION_STATE.SUBMITTING) {
-			setTimeout(() => {
-				setCurrentCharacterIndex(currentCharacterIndex + 1);
-				setInputValue(currentInputPlayer.name.slice(0, currentCharacterIndex + 1));
-				setCurrentCharacterTimeout(getRandomInputTime());
-			}, currentCharacterTimeout);
-		}
+			setCurrentState(EXECUTION_STATE.PRE_SUBMIT_WAITING);
+			setCurrentInputPlayer(newShuffledPlayers[0]);
+			setCurrentShuffledPlayers(newShuffledPlayers);
+			setCurrentTick(15);
+			setCurrentCharacterIndex(0);
+			setInputValue('');
+		},
+		[ currentShuffledPlayers ]
+	);
 
-		return (
-			<div className={outerClassName}>
-				{currentState === EXECUTION_STATE.START && (
-					<p>
-						In a moment, each player's name will be input into the input box one at a time. If you see a
-						green screen after your name has been entered you are safe for this episode. If, however, you
-						see a red screen after your name has been entered then you have been eliminated from the game.
-					</p>
-				)}
-				{_.includes(
-					[
-						EXECUTION_STATE.PRE_SUBMIT_WAITING,
-						EXECUTION_STATE.SUBMITTING,
-						EXECUTION_STATE.POST_SUBMIT_WAITING
-					],
-					currentState
-				) && <input type="text" ref={(c) => (this.myInputRef = c)} value={inputValue} />}
-			</div>
-		);
-	}
+	useEffect(
+		() => {
+			const timer = setTimeout(() => {
+				if (currentTick < 15) {
+					setCurrentState(EXECUTION_STATE.START);
+				} else if (currentTick >= 15 && currentTick < 20) {
+					setCurrentState(EXECUTION_STATE.PRE_SUBMIT_WAITING);
+				} else if (currentTick >= 20 && currentTick < 30) {
+					setCurrentState(EXECUTION_STATE.SUBMITTING);
+				} else if (currentTick >= 30 && currentTick < 35) {
+					setCurrentState(EXECUTION_STATE.POST_SUBMIT_WAITING);
+				} else if (currentTick >= 35 && currentTick < 40) {
+					setCurrentState(EXECUTION_STATE.LOADING_RESULTS);
+				} else if (currentTick >= 40 && currentTick < 45) {
+					setCurrentState(EXECUTION_STATE.SHOWING_RESULTS);
+				} else if (currentTick >= 45) {
+					setNextPlayer();
+				}
+
+				if (currentState !== EXECUTION_STATE.SUBMITTING && currentTick < 45) {
+					setCurrentTick(currentTick + 1);
+				}
+			}, 1000);
+			return () => clearTimeout(timer);
+		},
+		[ currentState, currentTick, setNextPlayer ]
+	);
+
+	useEffect(
+		() => {
+			if (currentState === EXECUTION_STATE.SUBMITTING) {
+				if (currentCharacterIndex >= currentInputPlayer.name.length) {
+					setCurrentTick(30);
+				} else {
+					const randomInterval = CHAR_INPUT_TIMES[_.random(CHAR_INPUT_TIMES.length)];
+					const timer = setTimeout(() => {
+						setInputValue(currentInputPlayer.name.slice(0, currentCharacterIndex + 1));
+						setCurrentCharacterIndex(currentCharacterIndex + 1);
+					}, randomInterval);
+					return () => clearTimeout(timer);
+				}
+			}
+		},
+		[ currentCharacterIndex, currentInputPlayer.name, currentState, setNextPlayer ]
+	);
+
+	return (
+		<div
+			className={clsx([
+				styles.execution,
+				currentState === EXECUTION_STATE.SHOWING_RESULTS && styles['execution-end'],
+				eliminatedPlayer.name === currentInputPlayer.name && styles.fail,
+				eliminatedPlayer.name !== currentInputPlayer.name && styles.success
+			])}
+		>
+			{currentTick}
+			{showStartParagraph && (
+				<p>
+					In a moment, each player's name will be input into the input box one at a time. If you see a green
+					screen after your name has been entered you are safe for this episode. If, however, you see a red
+					screen after your name has been entered then you have been eliminated from the game.
+				</p>
+			)}
+			{showInput && <input className={styles.input} type="text" value={inputValue} />}
+		</div>
+	);
 };
 
 export default ExecutionView;
