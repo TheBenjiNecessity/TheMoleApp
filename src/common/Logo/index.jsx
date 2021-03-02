141bec9297ca3c6219bc32fe21af9ce65a0d0766
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2021-03-01 23:21:48 -0500

    Add logo component

---
+++ src/common/Logo/index.jsx
@@ -0,0 +1,28 @@
+import { Fingerprint } from '@material-ui/icons';
+import styles from './styles.module.scss';
+
+const Logo = ({ size, textSize, withTitle, logoColor, textColor }) => {
+	const moleTextSize = parseInt(textSize);
+	const theTextSize = Math.floor(moleTextSize * 0.8);
+
+	return (
+		<div className={styles.logo}>
+			{withTitle && (
+				<div className={styles.title} style={{ color: textColor }}>
+					<span className={styles.the} style={{ fontSize: theTextSize + 'px' }}>
+						The
+					</span>
+					<br />
+					<span className={styles.mole} style={{ fontSize: moleTextSize + 'px' }}>
+						Mole
+					</span>
+				</div>
+			)}
+			<div className={styles.svg} style={{ color: logoColor }}>
+				<Fingerprint style={{ fontSize: size }} />
+			</div>
+		</div>
+	);
+};
+
+export default Logo;
