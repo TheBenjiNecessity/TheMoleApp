cb958f409e424275dfa93f475e4d57638a435a4f
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2021-03-11 11:55:02 -0500

    Add gameview test

--- src/__tests__/GameView.test.js
+++ src/__tests__/GameView.test.js
@@ -2,12 +2,27 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import GameView from '../components/GameView';
 import '../locales/i18n';
+import LobbyView from '../components/steps/LobbyView';
+import roomSample from './samples/room.sample';
 
 describe('MyComponent', () => {
+	let room = {};
+
+	beforeEach(() => {
+		room = roomSample.sampleRoom();
+	});
+
 	it('should render correctly', () => {
-		const room = {};
 		const component = shallow(<GameView room={room} />);
 
 		expect(component).toMatchSnapshot();
+	});
+
+	it('should render lobby', () => {
+		room.state = 'lobby';
+		const component = shallow(<GameView room={room} />);
+		const lobbyComp = component.find('[data-testid="lobby"]');
+
+		expect(lobbyComp.type()).toEqual(LobbyView);
 	});
 });
