e772dd3f6c6c2940480f8a0106a45913c9408092
TheBenjiNecessity <benjamin.wishart@gmail.com>
2021-03-15 15:08:17 -0400

    Add top bar to App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import { Routes } from './Routes';
 
 import './locales/i18n';
 import './App.scss';
+import Logo from './common/Logo';
 
 const App = () => {
-	return <Routes />;
+	return (
+		<div className="main">
+			<div className="top-bar">
+				<div id="topbar" className="title" />
+				<Logo size={200} textSize={45} />
+			</div>
+
+			<Routes />
+		</div>
+	);
 };
 
 export default App;
