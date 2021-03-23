51a637a5fa77dc71980dd497f5cc53df8b1b529e
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2021-03-22 22:22:48 -0400

    Add testid tags

--- src/components/GameView.jsx
+++ src/components/GameView.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import WelcomeView from './steps/WelcomeView';
 import EpisodeStartView from './steps/EpisodeStartView';
 import ChallengeView from './steps/ChallengeView';
@@ -17,25 +17,25 @@ const GameView = ({ room }) => {
 		case Room.ROOM_STATES.LOBBY:
 			return <LobbyView room={room} data-testid="lobby" />;
 		case Room.ROOM_STATES.WELCOME:
-			return <WelcomeView room={room} />;
+			return <WelcomeView room={room} data-testid="game-welcome" />;
 		case Room.ROOM_STATES.MOLE_REVEAL:
-			return <MoleRevealView />;
+			return <MoleRevealView data-testid="mole-reveal" />;
 		case Room.ROOM_STATES.EPISODE_START:
-			return <EpisodeStartView room={room} />;
+			return <EpisodeStartView room={room} data-testid="episode-start" />;
 		case Room.ROOM_STATES.IN_CHALLENGE:
-			return <ChallengeView room={room} />;
+			return <ChallengeView room={room} data-testid="in-challenge" />;
 		case Room.ROOM_STATES.CHALLENGE_INTERMISSION:
-			return <ChallengeIntermissionView room={room} />;
+			return <ChallengeIntermissionView room={room} data-testid="challenge-intermission" />;
 		case Room.ROOM_STATES.PRE_QUIZ_INTERMISSION:
-			return <PreQuizIntermissionView room={room} />;
+			return <PreQuizIntermissionView room={room} data-testid="pre-quiz-intermission" />;
 		case Room.ROOM_STATES.IN_QUIZ:
-			return <QuizView room={room} />;
+			return <QuizView room={room} data-testid="in-quiz" />;
 		case Room.ROOM_STATES.POST_QUIZ_INTERMISSION:
-			return <PostQuizIntermissionView room={room} />;
+			return <PostQuizIntermissionView room={room} data-testid="post-quiz-intermission" />;
 		case Room.ROOM_STATES.EXECUTION:
-			return <ExecutionView room={room} />;
+			return <ExecutionView room={room} data-testid="execution" />;
 		case Room.ROOM_STATES.EXECUTION_WRAPUP:
-			return <ExecutionWrapupView room={room} />;
+			return <ExecutionWrapupView room={room} data-testid="execution-wrapup" />;
 		default:
 			return null;
 	}
