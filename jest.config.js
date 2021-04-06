42643a7e2194edfc58afe32d82b5f6f8ff12b436
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2021-04-06 19:22:39 -0400

    Change how classes show in tests

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
 	moduleNameMapper: {
 		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
 			'<rootDir>/src/__mocks__/fileMock.js',
-		'\\.(css|sass|scss)$': '<rootDir>/src/__mocks__/styleMock.js'
+		'\\.(css|sass|scss)$': 'identity-obj-proxy'
 	},
 	snapshotSerializers: [ 'enzyme-to-json/serializer' ]
 };
