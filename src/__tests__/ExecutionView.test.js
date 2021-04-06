4a5cd3fabe344c1cfae60b7ea678c672eb7c695d
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2021-04-06 19:22:01 -0400

    Add execution view tests

--- src/__tests__/ExecutionView.test.js
+++ src/__tests__/ExecutionView.test.js
@@ -1,7 +1,63 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 import '../locales/i18n';
 
-import ExecutionView from '../components/steps/ExecutionView';
+import View, { EXECUTION_STATE } from '../components/steps/ExecutionView/view';
 
-describe('ExecutionView', () => {});
+describe('ExecutionView', () => {
+	it('should render execution view correctly {currentState: EXECUTION_STATE.START, wasExecuted=false inputValue=test}', () => {
+		const mountedComponent = shallow(
+			<View currentState={EXECUTION_STATE.START} wasExecuted={false} inputValue={'test'} />
+		);
+
+		expect(mountedComponent).toMatchSnapshot();
+	});
+
+	it('should render execution view correctly {currentState: EXECUTION_STATE.PRE_SUBMIT_WAITING, wasExecuted=false inputValue=test}', () => {
+		const mountedComponent = shallow(
+			<View currentState={EXECUTION_STATE.PRE_SUBMIT_WAITING} wasExecuted={false} inputValue={'test'} />
+		);
+
+		expect(mountedComponent).toMatchSnapshot();
+	});
+
+	it('should render execution view correctly {currentState: EXECUTION_STATE.SUBMITTING, wasExecuted=false inputValue=test}', () => {
+		const mountedComponent = shallow(
+			<View currentState={EXECUTION_STATE.SUBMITTING} wasExecuted={false} inputValue={'test'} />
+		);
+
+		expect(mountedComponent).toMatchSnapshot();
+	});
+
+	it('should render execution view correctly {currentState: EXECUTION_STATE.POST_SUBMIT_WAITING, wasExecuted=false inputValue=test}', () => {
+		const mountedComponent = shallow(
+			<View currentState={EXECUTION_STATE.POST_SUBMIT_WAITING} wasExecuted={false} inputValue={'test'} />
+		);
+
+		expect(mountedComponent).toMatchSnapshot();
+	});
+
+	it('should render execution view correctly {currentState: EXECUTION_STATE.LOADING_RESULTS, wasExecuted=false inputValue=test}', () => {
+		const mountedComponent = shallow(
+			<View currentState={EXECUTION_STATE.LOADING_RESULTS} wasExecuted={false} inputValue={'test'} />
+		);
+
+		expect(mountedComponent).toMatchSnapshot();
+	});
+
+	it('should render execution view correctly {currentState: EXECUTION_STATE.SHOWING_RESULTS, wasExecuted=false inputValue=test}', () => {
+		const mountedComponent = shallow(
+			<View currentState={EXECUTION_STATE.SHOWING_RESULTS} wasExecuted={false} inputValue={'test'} />
+		);
+
+		expect(mountedComponent).toMatchSnapshot();
+	});
+
+	it('should render execution view correctly {currentState: EXECUTION_STATE.SHOWING_RESULTS, wasExecuted=true inputValue=test}', () => {
+		const mountedComponent = shallow(
+			<View currentState={EXECUTION_STATE.SHOWING_RESULTS} wasExecuted={true} inputValue={'test'} />
+		);
+
+		expect(mountedComponent).toMatchSnapshot();
+	});
+});
