9f520becd6a122a6282d87913c2b5180c580f123
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2021-05-02 20:19:29 -0400

    Use new Story comp

--- src/components/steps/ExecutionView/index.jsx
+++ src/components/steps/ExecutionView/index.jsx
@@ -1,119 +1,148 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import * as _ from 'lodash';
-import roomSocketService from '../../../services/socket-services/room-socket.service';
-import View from './view';
+import Story from '../../../common/Story';
+import AutoTextInput from '../../../common/AutoTextInput';
 
-const EXECUTION_STATE = {
-	START: 0, // Before showing the player input (Showing a message about seeing red/green screen)
-	PRE_SUBMIT_WAITING: 1, // Player input is showing but typing hasn't started
-	SUBMITTING: 2, // Characters are being typed into the input
-	POST_SUBMIT_WAITING: 3, // Typing has finished but input hasn't been submitted yet
-	LOADING_RESULTS: 4, // Typing has finished and the input has been submited hiding the input (waiting for result)
-	SHOWING_RESULTS: 5 // The Red/Green results screen is showing
+import styles from './styles.module.scss';
+import { useTranslation } from 'react-i18next';
+import Logo from '../../../common/Logo';
+import clsx from 'clsx';
+
+const EXECUTION_CHAPTERS = {
+	INSTRUCTIONS: 0,
+	INPUT: 1,
+	SUBMITTED_DELAY: 2,
+	SUCCESS: 3,
+	FAIL: 4
 };
 
-const CHAR_INPUT_TIMES = [ 900, 750, 1000, 800 ];
-
 const ExecutionView = ({ roomcode, shuffledPlayers, eliminatedPlayer }) => {
-	const allBeforeEl = useMemo(
+	const { t } = useTranslation('execution_view');
+
+	const [ activate ] = useState(true);
+	const [ activatePlayerInput, setActivatePlayerInput ] = useState(false);
+	const [ resetPlayerInput, setResetPlayerInput ] = useState(false);
+	const [ shouldGoNext, setShouldGoNext ] = useState(false);
+	const [ currentInputPlayerIndex, setCurrentInputPlayerIndex ] = useState(0);
+	const [ shouldShowLogo, setShouldShowLogo ] = useState(true);
+
+	const currentInputPlayer = useMemo(
 		() => {
-			return _.slice(
-				shuffledPlayers,
-				0,
-				_.findIndex(shuffledPlayers, (player) => player.name === eliminatedPlayer.name) + 1
+			return shuffledPlayers[currentInputPlayerIndex];
+		},
+		[ currentInputPlayerIndex, shuffledPlayers ]
+	);
+
+	function didEnd() {
+		console.log('didEnd');
+	}
+
+	const didGoNext = useCallback(
+		(previousChapterIndex, nextChapterIndex) => {
+			setShouldShowLogo(
+				[
+					EXECUTION_CHAPTERS.INSTRUCTIONS,
+					EXECUTION_CHAPTERS.INPUT,
+					EXECUTION_CHAPTERS.SUBMITTED_DELAY
+				].indexOf(nextChapterIndex) >= 0
 			);
-		},
-		[ eliminatedPlayer, shuffledPlayers ]
-	);
 
-	const [ currentShuffledPlayers, setCurrentShuffledPlayers ] = useState(allBeforeEl);
-	const [ currentInputPlayer, setCurrentInputPlayer ] = useState(shuffledPlayers[0]);
-	const [ currentCharacterIndex, setCurrentCharacterIndex ] = useState(-1);
-	const [ currentTick, setCurrentTick ] = useState(0);
-	const [ currentState, setCurrentState ] = useState(EXECUTION_STATE.START);
-	const [ inputValue, setInputValue ] = useState('');
-	const [ cursor, setCursor ] = useState('|');
-
-	const setNextPlayer = useCallback(
-		() => {
-			if (currentShuffledPlayers.length >= 2) {
-				const newShuffledPlayers = currentShuffledPlayers.slice(1);
-
-				setCurrentState(EXECUTION_STATE.PRE_SUBMIT_WAITING);
-				setCurrentInputPlayer(newShuffledPlayers[0]);
-				setCurrentShuffledPlayers(newShuffledPlayers);
-				setCurrentTick(15);
-				setCurrentCharacterIndex(0);
-				setInputValue('');
+			if (nextChapterIndex === EXECUTION_CHAPTERS.INPUT) {
+				setResetPlayerInput(true);
+				setActivatePlayerInput(true);
 			} else {
-				roomSocketService.moveNext(roomcode);
+				setResetPlayerInput(false);
+				setActivatePlayerInput(false);
+			}
+
+			if (nextChapterIndex === EXECUTION_CHAPTERS.SUCCESS) {
+				setCurrentInputPlayerIndex(currentInputPlayerIndex + 1);
 			}
 		},
-		[ currentShuffledPlayers, roomcode ]
+		[ currentInputPlayerIndex ]
 	);
 
-	useEffect(
-		() => {
-			const timer = setTimeout(() => {
-				if (currentTick < 15) {
-					setCurrentState(EXECUTION_STATE.START);
-				} else if (currentTick >= 15 && currentTick < 20) {
-					setCurrentState(EXECUTION_STATE.PRE_SUBMIT_WAITING);
-				} else if (currentTick >= 20 && currentTick < 30) {
-					setCurrentState(EXECUTION_STATE.SUBMITTING);
-				} else if (currentTick >= 30 && currentTick < 35) {
-					setCurrentState(EXECUTION_STATE.POST_SUBMIT_WAITING);
-				} else if (currentTick >= 35 && currentTick < 40) {
-					setCurrentState(EXECUTION_STATE.LOADING_RESULTS);
-				} else if (currentTick >= 40 && currentTick < 45) {
-					setCurrentState(EXECUTION_STATE.SHOWING_RESULTS);
-				} else if (currentTick >= 45) {
-					setNextPlayer();
-				}
+	const getNextIndex = useCallback(
+		(stepIndex) => {
+			if (stepIndex === EXECUTION_CHAPTERS.INSTRUCTIONS) {
+				return EXECUTION_CHAPTERS.INPUT;
+			}
 
-				if (currentState !== EXECUTION_STATE.SUBMITTING && currentTick < 45) {
-					setCurrentTick(currentTick + 1);
-				}
-			}, 1000);
-			return () => clearTimeout(timer);
-		},
-		[ currentState, currentTick, setNextPlayer ]
-	);
+			if (stepIndex === EXECUTION_CHAPTERS.INPUT) {
+				return EXECUTION_CHAPTERS.SUBMITTED_DELAY;
+			}
 
-	useEffect(
-		() => {
-			if (currentState === EXECUTION_STATE.SUBMITTING) {
-				if (currentCharacterIndex >= currentInputPlayer.name.length) {
-					setCurrentTick(30);
+			if (stepIndex === EXECUTION_CHAPTERS.SUBMITTED_DELAY) {
+				if (currentInputPlayer.name === eliminatedPlayer.name) {
+					return EXECUTION_CHAPTERS.FAIL;
 				} else {
-					const randomInterval = CHAR_INPUT_TIMES[_.random(CHAR_INPUT_TIMES.length)];
-					const timer = setTimeout(() => {
-						setInputValue(currentInputPlayer.name.slice(0, currentCharacterIndex + 1));
-						setCurrentCharacterIndex(currentCharacterIndex + 1);
-					}, randomInterval);
-					return () => clearTimeout(timer);
+					return EXECUTION_CHAPTERS.SUCCESS;
 				}
 			}
+
+			if (stepIndex === EXECUTION_CHAPTERS.SUCCESS) {
+				return EXECUTION_CHAPTERS.INPUT;
+			}
+
+			return null;
 		},
-		[ currentCharacterIndex, currentInputPlayer.name, currentState, setNextPlayer ]
+		[ currentInputPlayer.name, eliminatedPlayer.name ]
 	);
 
-	useEffect(
-		() => {
-			const timer = setTimeout(() => {
-				setCursor(cursor === '|' ? '' : '|');
-			}, 500);
-			return () => clearTimeout(timer);
-		},
-		[ cursor ]
-	);
+	const inputDone = useCallback(() => {
+		setResetPlayerInput(true);
+		setActivatePlayerInput(false);
+		setShouldGoNext(true);
+	}, []);
 
 	return (
-		<View
-			currentState={currentState}
-			wasExecuted={eliminatedPlayer.name === currentInputPlayer.name}
-			inputValue={`${inputValue}${cursor}`}
-		/>
+		<div className={styles.execution}>
+			<div className="panel abs-centered-panel hv-centered-panel">
+				<Story
+					activate={activate}
+					goNext={shouldGoNext}
+					resetNext={() => setShouldGoNext(false)}
+					didGoNext={didGoNext}
+					didEnd={didEnd}
+					getNextIndex={getNextIndex}
+				>
+					<Story.Chapter time={3000}>
+						<div className={styles['start-paragraph']}>
+							<p>{t('first_paragraph')}</p>
+						</div>
+					</Story.Chapter>
+					<Story.Chapter>
+						<label className={styles.inputLabel}>
+							<span className={styles.text}>Name:</span>
+							<span className={styles.input}>
+								<AutoTextInput
+									value={currentInputPlayer.name}
+									reset={resetPlayerInput}
+									onDone={inputDone}
+									activate={activatePlayerInput}
+								/>
+							</span>
+						</label>
+					</Story.Chapter>
+					<Story.Chapter time={3000} />
+					<Story.Chapter time={5000}>
+						<div className={styles.success}>
+							<Logo size={400} logoColor="white" />
+						</div>
+					</Story.Chapter>
+					<Story.Chapter time={5000}>
+						<div className={styles.fail}>
+							<Logo size={400} logoColor="white" />
+						</div>
+					</Story.Chapter>
+				</Story>
+			</div>
+			{shouldShowLogo && (
+				<div className={clsx([ styles.logo, styles['corner-logo'] ])}>
+					<Logo size={200} textSize={45} />
+				</div>
+			)}
+		</div>
 	);
 };
 
