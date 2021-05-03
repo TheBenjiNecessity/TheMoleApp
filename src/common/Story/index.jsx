177e0c01820e43df8fda651ba3b0906319e19eb4
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2021-05-02 20:18:44 -0400

    Fix Story component

--- src/common/Story/index.jsx
+++ src/common/Story/index.jsx
@@ -1,55 +1,60 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import Chapter from './Chapter';
 
 /**
  * Show a series of 
- * @param {*} param0 
+ * @param {boolean} activate - Story will only execute if activate is true, will pause if false.
+ * @param {boolean} goNext - A message from parent to go to the next step programmatically.
+ * @param {callback} didGoNext - Called whenever this component goes to the next step (sends parameters for previous stepIndex and next stepIndex)
+ * @param {callback} didEnd - Called when the story is over
+ * @param {callback} getNextIndex - Function provided by the parent component telling Story what index to go to on moving next.
  * @returns 
  */
-const Story = ({ start, goNext, onNext, onEnd, whereNext = (index) => index + 1, children }) => {
+
+const Story = ({ activate, goNext, resetNext, didGoNext, didEnd, getNextIndex = (index) => index + 1, children }) => {
 	const childArray = React.Children.toArray(children);
-	const [ hasStarted, setHasStarted ] = useState(false);
 	const [ currentChapterIndex, setCurrentChapterIndex ] = useState(0);
 
-	// Check if the story has started based on a message from parent
-	useEffect(
+	const goToNextStep = useCallback(
 		() => {
-			if (start) {
-				setHasStarted(true);
+			const nextChapterIndex = getNextIndex(currentChapterIndex);
+
+			if (!nextChapterIndex || nextChapterIndex >= childArray.length) {
+				didEnd();
+			} else {
+				setCurrentChapterIndex(nextChapterIndex);
+				didGoNext(currentChapterIndex, nextChapterIndex);
 			}
 		},
-		[ start ]
+		[ childArray.length, currentChapterIndex, didEnd, didGoNext, getNextIndex ]
 	);
 
-	// Move to the next step based on a message from parent
 	useEffect(
 		() => {
 			if (goNext) {
-				const nextStepIndex = whereNext(currentChapterIndex);
-				if (nextStepIndex < childArray.length - 1) {
-					setCurrentChapterIndex(whereNext(currentChapterIndex));
-					onNext(false);
-				} else {
-					onEnd();
-				}
+				goToNextStep();
+				resetNext();
 			}
 		},
-		[ childArray, currentChapterIndex, goNext, onEnd, onNext, whereNext ]
+		[ goNext, goToNextStep, resetNext ]
 	);
 
 	// Start the next timer if there is one
 	useEffect(
 		() => {
 			const { time } = childArray[currentChapterIndex].props;
-			if (hasStarted && time) {
+
+			if (activate && time) {
 				const timer = setTimeout(() => {
-					onNext(true);
+					goToNextStep();
 				}, time);
 				return () => clearTimeout(timer);
 			}
+
+			return () => {};
 		},
-		[ childArray, currentChapterIndex, hasStarted, onNext ]
+		[ activate, childArray, currentChapterIndex, goToNextStep ]
 	);
 
 	return childArray[currentChapterIndex];
